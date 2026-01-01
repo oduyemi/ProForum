e29843703f1998bb14823b13cd75beee9b5e804d
Yemi Oduyemi <yhermii@gmail.com>
2026-01-01 13:45:30 +0100

    updated mail configurations

--- helper/SendEmail.ts
+++ helper/SendEmail.ts
@@ -1,29 +1,14 @@
-import nodemailer from "nodemailer";
-
-export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT || 465,
-  secure: true,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  pool: true,      
-  maxConnections: 5,
-  maxMessages: 100,
-});
-
+import { transporter } from "./transporter";
 
 export const sendEmail = async (
-    to: string,
-    subject: string,
-    html: string
-  ) => {
-    await transporter.sendMail({
-      from: `"ProGrowing Forum" <no-reply@progrowing.com>`,
-      to,
-      subject,
-      html,
-    });
-  };
-  
+  to: string,
+  subject: string,
+  html: string
+) => {
+  await transporter.sendMail({
+    from: `"ProGrowing Forum" <no-reply@progrowing.com>`,
+    to,
+    subject,
+    html,
+  });
+};
